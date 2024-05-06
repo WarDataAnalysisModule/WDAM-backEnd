fdf8229ffc89d08960eaa6a1880d20a65809551c
kyeongjeong <ariel0918@kw.ac.kr>
2024-05-06 22:25:30 +0900

    Feat: 분석 결과 get(모듈과 연결 x, 토큰으로 유저 정보 x)

---
+++ wdam/src/main/java/com/back/wdam/analyze/service/AnalyzeService.java
@@ -0,0 +1,54 @@
+package com.back.wdam.analyze.service;
+
+import com.back.wdam.analyze.dto.AnalyzeResultDto;
+import com.back.wdam.analyze.repository.AnalyzeRepository;
+import com.back.wdam.entity.ResultLog;
+import com.back.wdam.entity.Users;
+import com.back.wdam.user.repository.UserRepository;
+import com.back.wdam.util.exception.CustomException;
+import com.back.wdam.util.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class AnalyzeService {
+
+    private final UserRepository userRepository;
+    private final AnalyzeRepository analyzeRepository;
+
+    public Long saveNewAnalyzeResult(Long userIdx, String analysisFeature, String result, LocalDateTime logCreated) {
+
+        Users users = getUserById(userIdx);
+        ResultLog resultLog = analyzeRepository.saveAndFlush(new ResultLog(users, analysisFeature, result, logCreated));
+        return resultLog.getLogIdx();
+    }
+
+    public List<AnalyzeResultDto> getAnalyzeResults(Long userIdx, LocalDateTime logCreated) {
+
+        List<ResultLog> resultLogs = analyzeRepository.findByUserIdAndLogCreated(userIdx, logCreated);
+        if(resultLogs.isEmpty())
+            throw new CustomException(ErrorCode.RESULTLOG_NOT_FOUND);
+
+        List<AnalyzeResultDto> analyzeResultDtos = new ArrayList<>();
+        for(ResultLog resultLog : resultLogs) {
+            analyzeResultDtos.add(new AnalyzeResultDto(resultLog));
+        }
+        return analyzeResultDtos;
+    }
+
+    private Users getUserById(Long userIdx) {
+
+        Optional<Users> users = userRepository.findByUserIdx(userIdx);
+        if(users.isEmpty())
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        return users.get();
+    }
+}
