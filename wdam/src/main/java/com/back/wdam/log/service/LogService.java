97ae871564bf3bec02809cdc97a560202fe9c3a2
kyeongjeong <ariel0918@kw.ac.kr>
2024-05-17 16:29:38 +0900

    Feat: log API에 unit_list 보내도록 추가

--- wdam/src/main/java/com/back/wdam/log/service/LogService.java
+++ wdam/src/main/java/com/back/wdam/log/service/LogService.java
@@ -1,17 +1,16 @@
 package com.back.wdam.log.service;
 
 import com.back.wdam.entity.ResultLog;
+import com.back.wdam.entity.UnitList;
 import com.back.wdam.entity.Users;
 import com.back.wdam.file.repository.UnitListRepository;
 import com.back.wdam.log.dto.LogDto;
 import com.back.wdam.log.dto.LogResultDto;
-import com.back.wdam.log.dto.UnitListDto;
 import com.back.wdam.log.repository.LogRepository;
 import com.back.wdam.user.repository.UserRepository;
 import com.back.wdam.util.exception.CustomException;
 import com.back.wdam.util.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -43,8 +42,11 @@ public class LogService {
             logResultDtos.add(new LogResultDto(resultLog));
         }
 
-        List<UnitListDto> unitListDtos;
-        LogDto logDto;
+        Optional<List<UnitList>> unitList = unitListRepository.findAllByUserIdxAndLogCreated(user.getUserIdx(), logCreated);
+        if(unitList.isEmpty() || unitList.get().isEmpty())
+            throw new CustomException(ErrorCode.UNIT_LIST_NOT_FOUND);
+
+        LogDto logDto = new LogDto(unitList.get(), logResultDtos);
         return logDto;
     }
 
